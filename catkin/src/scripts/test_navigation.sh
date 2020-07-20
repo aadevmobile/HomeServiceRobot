9583d2c65fb47c52ddb180dccc3c680134e2502f
aadevmobile <aadevmobile@gmail.com>
2020-07-20 17:11:21 +0000

    test_slam, test_navigation working. map needs adjustments

--- catkin/src/scripts/test_navigation.sh
+++ catkin/src/scripts/test_navigation.sh
@@ -1,1 +1,9 @@
-blah
+#!/bin/sh
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/myhsrworld.world" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch "&
+#xterm  -e  " rosrun rviz rviz -d /home/workspace/HomeServiceRobot/catkin/src/rvizConfig/rvizConfig.rviz"&
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HomeServiceRobot/catkin/src/map/map.yaml "
+
--- catkin/src/scripts/test_slam.sh
+++ catkin/src/scripts/test_slam.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-xterm  -e  " turtlebot_world.launch " &
+
+
+xterm  -e  " roslaunch turtlebot_gazebo  turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/myhsrworld.world " &
+#xterm  -e  " roslaunch my_robot  world.launch " &
 sleep 5
-xterm  -e  " gmapping_demo.launch" & 
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+#xterm  -e  " rosrun gmapping slam_gmapping scan:=scan " &
 sleep 5
-xterm  -e  " view_navigation.launch " &
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch "&
+#xterm  -e  " roslaunch /home/workspace/HomeServiceRobot/catkin/src/view_navigation.launch "&
 sleep 5
-xterm  -e  " keyboard_teleop.launch "
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
+#xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py "
