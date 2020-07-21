baf18edc81913676467beb2052f76ee776e8f7ee
aadevmobile <aadevmobile@gmail.com>
2020-07-21 22:53:55 +0000

    fixed add_markers and home_service scripts

--- catkin/src/scripts/add_markers.sh
+++ catkin/src/scripts/add_markers.sh
@@ -7,5 +7,4 @@ sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HomeServiceRobot/catkin/src/map/map.yaml " &
 sleep 5
 xterm  -e  " source devel/setup.bash; rosrun add_markers add_markers " &
-sleep 5
-xterm  -e  " source devel/setup.bash; rosrun pick_objects pick_objects " &
+
--- catkin/src/scripts/home_service.sh
+++ catkin/src/scripts/home_service.sh
@@ -3,9 +3,9 @@
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/myhsrworld.world" &
 sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch "&
-#xterm  -e  " rosrun rviz rviz -d /home/workspace/HomeServiceRobot/catkin/src/rvizConfig/rvizConfig.rviz"&
 sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/HomeServiceRobot/catkin/src/map/map.yaml " &
-
 sleep 5
-xterm  -e  " source devel/setup.bash; rosrun add_markers add_markers "
+xterm  -e  " source devel/setup.bash; rosrun add_markers add_markers " &
+sleep 5
+xterm  -e  " source devel/setup.bash; rosrun pick_objects pick_objects " &
